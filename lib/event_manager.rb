f1e71b90b52b51744eef611e7831f1feaaaf9bd2
Marc Bleeze <marcyb@users.noreply.github.com>
2025-02-04 12:18:59 +0000

    Add phone number cleaning method
    Cleans phone numbers according to follwoing rules:
     - If the phone number is less than 10 digits, assume that it is a bad number
     - If the phone number is 10 digits, assume that it is good
     - If the phone number is 11 digits and the first number is 1, trim the 1 and use the remaining 10 digits
     - If the phone number is 11 digits and the first number is not 1, then it is a bad number
     - If the phone number is more than 11 digits, assume that it is a bad number

--- lib/event_manager.rb
+++ lib/event_manager.rb
@@ -11,6 +11,10 @@ def clean_zipcode(zipcode)
   zipcode.to_s.rjust(5, '0')[0..4]
 end
 
+def clean_phone_number(str)
+  /^1?(\d{10})$/.match(str.gsub(/\D/, ''))&.captures
+end
+
 def legislators_by_zipcode(zip)
   civic_info = Google::Apis::CivicinfoV2::CivicInfoService.new
   civic_info.key = 'AIzaSyClRzDqDh5MsXwnCWi0kOiiBivP6JsSyBw'
@@ -36,15 +40,28 @@ puts 'EventManager initialized.'
 
 contents = CSV.open('event_attendees.csv', headers: true, header_converters: :symbol)
 
-template_letter = File.read('form_letter.erb')
-erb_template = ERB.new template_letter
+# Create thank you letters
+def create_thank_you_letters(contents)
+  template_letter = File.read('form_letter.erb')
+  erb_template = ERB.new template_letter
 
-contents.each do |row|
-  id = row[0]
-  name = row[:first_name]
-  zipcode = clean_zipcode(row[:zipcode])
-  legislators = legislators_by_zipcode(zipcode)
+  contents.each do |row|
+    id = row[0]
+    name = row[:first_name]
+    zipcode = clean_zipcode(row[:zipcode])
+    legislators = legislators_by_zipcode(zipcode)
 
-  form_letter = erb_template.result(binding)
-  save_thank_you_letter(id, form_letter)
+    form_letter = erb_template.result(binding)
+    save_thank_you_letter(id, form_letter)
+  end
 end
+
+# Assignment part 1
+def clean_phone_numbers(contents)
+  contents.each do |row|
+    puts clean_phone_number(row[:homephone])
+  end
+end
+
+# create_thank_you_letters(contents)
+clean_phone_numbers(contents)
